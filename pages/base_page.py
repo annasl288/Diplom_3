f3074e91a2c10b261a725e0340393f817247a4e4
annasl288 <anne.slobodyanyuk@gmail.com>
2025-04-17 14:44:06 +0300

    Добавлены allure-аннотации для методов в base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,34 +1,44 @@
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from locators import MainPageLocators
+import allure
 
 class BasePage:
 
     def __init__(self, driver):
-        self.driver = driver
+        with allure.step('Запустить браузер'):
+            self.driver = driver
 
     def go_to_url(self, url):
-        self.driver.get(url)
+        with allure.step(f'Перейти по ссылке {url}'):
+            self.driver.get(url)
 
     def get_element(self, locator):
-        return self.driver.find_element(*locator)
+        with allure.step(f'Найти элемент {locator}'):
+            return self.driver.find_element(*locator)
 
     def get_elements(self, locator):
-        return self.driver.find_elements(*locator)
+        with allure.step(f'Найти элементы {locator}'):
+            return self.driver.find_elements(*locator)
 
     def wait_for_element(self, locator):
-        WebDriverWait(self.driver, 15).until(expected_conditions.visibility_of_element_located(locator))
-        return self.get_element(locator)
+        with allure.step(f'Дождаться видимости элемента {locator}'):
+            WebDriverWait(self.driver, 15).until(expected_conditions.visibility_of_element_located(locator))
+            return self.get_element(locator)
 
     def wait_for_element_invisibility(self, locator):
-        WebDriverWait(self.driver, 15).until(expected_conditions.invisibility_of_element(locator))
+        with allure.step(f'Дождаться исчезновения элемента {locator}'):
+            WebDriverWait(self.driver, 15).until(expected_conditions.invisibility_of_element(locator))
 
     def wait_for_url(self, url):
-        WebDriverWait(self.driver, 5).until(expected_conditions.url_to_be(url))
+        with allure.step(f'Дождаться перехода к странице {url}'):
+            WebDriverWait(self.driver, 5).until(expected_conditions.url_to_be(url))
 
     def get_current_url(self):
-        return self.driver.current_url
+        with allure.step('Получить текущий url'):
+            return self.driver.current_url
 
     def click_element(self, locator):
-        self.wait_for_element_invisibility(MainPageLocators.OVERLAY)
-        self.wait_for_element(locator).click()
+        with allure.step(f'Кликнуть по элементу {locator}'):
+            self.wait_for_element_invisibility(MainPageLocators.OVERLAY)
+            self.wait_for_element(locator).click()
